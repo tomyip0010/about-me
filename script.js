77065be0cdff8463bdaf1fcbc2e1f3e670501a9d
Tom <tomyip0010@hotmail.com>
2017-10-08 13:35:58 +0800

    finish

--- script.js
+++ script.js
@@ -87,5 +87,16 @@ $(document).ready(function() {
 
     photoControl();
     smoothScroll();
+
+    $(window).scroll(function() {
+        $(".slideanim").each(function(){
+            var pos = $(this).offset().top;
+        
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 600) {
+            $(this).addClass("slide");
+            }
+        });
+    });
 })
 
